c755a641d0e273640f207a9fff8adfcad91b62cb
Valerius Petrini <valeriuspetrini@gmail.com>
2022-11-16 09:22:16 -0600

    Dropdown

--- src/TS/buttons.ts
+++ src/TS/buttons.ts
@@ -1,8 +1,9 @@
 let displayTranslationChart: boolean = false;
 
+const chartButton: Element = document.querySelector("#activateChart")!;
+const chart: Element = document.querySelector(".container")!;
+
 function clickDisplayTranslationChart() {
-    let chartButton: Element = document.querySelector("#activateChart")!;
-    let chart: Element = document.querySelector(".container")!;
     displayTranslationChart = !displayTranslationChart;
     chart.setAttribute("style", `display: ${displayTranslationChart ? "grid" : "none"};`)
     chartButton.innerHTML = displayTranslationChart ? "Hide Translation Chart" : "Show Translation Chart";
