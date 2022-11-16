595f8a1d2ef008b5eef4aacb93a394a56e0d2a72
Valerius Petrini <valeriuspetrini@gmail.com>
2022-11-16 09:40:10 -0600

    Hints for matching dna

--- src/JS/buttons.js
+++ src/JS/buttons.js
@@ -1,9 +1,26 @@
 "use strict";
 let displayTranslationChart = false;
+let displayHint = false;
+const chartButton = document.querySelector("#activateChart");
+const chart = document.querySelector(".container");
+const hintButton = document.querySelector("#activateHint");
+const hint = document.querySelector("#hint");
+const activitiesDropdown = document.getElementById("activities");
 function clickDisplayTranslationChart() {
-    let chartButton = document.querySelector("#activateChart");
-    let chart = document.querySelector(".container");
     displayTranslationChart = !displayTranslationChart;
     chart.setAttribute("style", `display: ${displayTranslationChart ? "grid" : "none"};`);
     chartButton.innerHTML = displayTranslationChart ? "Hide Translation Chart" : "Show Translation Chart";
+}
+function getHint() {
+    switch (activitiesDropdown.value) {
+        case "match-dna":
+            return "<pre>A turns to T\nT turns to A\nG turns to C\nC turns to G\n</pre>";
+        default:
+            throw new Error("ID not found for: " + activitiesDropdown.value);
+    }
+}
+function clickDisplayHint() {
+    displayHint = !displayHint;
+    hintButton.innerHTML = displayHint ? "Hide Hint" : "Show Hint";
+    hint.innerHTML = displayHint ? getHint() : "";
 }
--- src/TS/buttons.ts
+++ src/TS/buttons.ts
@@ -1,10 +1,31 @@
 let displayTranslationChart: boolean = false;
+let displayHint: boolean = false;
 
 const chartButton: Element = document.querySelector("#activateChart")!;
 const chart: Element = document.querySelector(".container")!;
 
-function clickDisplayTranslationChart() {
+const hintButton: Element = document.querySelector("#activateHint")!;
+const hint: Element = document.querySelector("#hint")!;
+
+const activitiesDropdown = document.getElementById("activities") as HTMLSelectElement;
+
+function clickDisplayTranslationChart(): void {
     displayTranslationChart = !displayTranslationChart;
     chart.setAttribute("style", `display: ${displayTranslationChart ? "grid" : "none"};`)
     chartButton.innerHTML = displayTranslationChart ? "Hide Translation Chart" : "Show Translation Chart";
+}
+
+function getHint(): string {
+    switch (activitiesDropdown.value) {
+        case "match-dna":
+            return "<pre>A turns to T\nT turns to A\nG turns to C\nC turns to G\n</pre>";
+        default:
+            throw new Error("ID not found for: " + activitiesDropdown.value);
+    }
+}
+
+function clickDisplayHint(): void {
+    displayHint = !displayHint;
+    hintButton.innerHTML = displayHint ? "Hide Hint" : "Show Hint";
+    hint.innerHTML = displayHint ? getHint() : "";
 }
